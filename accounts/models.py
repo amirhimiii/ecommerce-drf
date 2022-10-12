85655976a6de0455672d34b23d0fc1fe02d6f939
amirhossein <amirhossein.rahimi494@gmail.com>
2022-10-12 12:05:49 +0330

    add phone number

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True,max_length=254)
+    phone_number = PhoneNumberField(max_length=14,help_text='e.g:0912 123 4567',blank=True, null=True)
     is_author = models.BooleanField(_("is author?"), default=False)
 
