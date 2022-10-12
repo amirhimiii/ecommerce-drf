5d25d59310a664cd6bca66bdd6886789fcdd6114
amirhossein <amirhossein.rahimi494@gmail.com>
2022-10-12 10:32:53 +0330

    add checkout model

--- process/models.py
+++ process/models.py
@@ -39,3 +39,18 @@ class CartItem(models.Model):
          
     def get_image(self):
         return self.product.image.url
+
+
+class Checkout(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name='author' )
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE , related_name='checkouts') 
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    # phone_number = PhoneNumberField(max_length=14,help_text='e.g:0912 123 4567')
+    address = models.CharField( max_length=200, blank=False, null=False)
+    # country = CountryField(blank_label='(select country)', blank=False, null=False)
+    zip_code =models.IntegerField( blank=False, null=False)
+
+    def __str__(self):
+        return self.first_name
